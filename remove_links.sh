a82ae107aded9f90f1a6c604c8ffded6c36bc557
Marylia Gutierrez <marylia@cockroachlabs.com>
2021-04-23 14:45:19 -0400

    remove links

--- remove_links.sh
+++ remove_links.sh
@@ -2,8 +2,6 @@
 
 cd ~/go/src/github.com/cockroachdb/ui/packages/cluster-ui && yarn unlink "@cockroachlabs/crdb-protobuf-client"
 cd ~/go/src/github.com/cockroachdb/cockroach/pkg/ui && yarn unlink "@cockroachlabs/cluster-ui"
-cd ~/go/src/github.com/cockroachdb/ui/packages/cluster-ui
-yarn unlink
-yarn install --force
+cd ~/go/src/github.com/cockroachdb/ui/packages/cluster-ui && yarn unlink && yarn install --force
 cd ~/go/src/github.com/cockroachdb/cockroach/pkg/ui/src/js && yarn unlink
 cd ~/go/src/github.com/cockroachdb/cockroach/pkg/ui && yarn install --force
